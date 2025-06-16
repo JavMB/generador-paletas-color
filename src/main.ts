eb2ab13b2ac703733d0f93f4a3fa7e74ad563f0c
Javier  Mengual <114607632+JavMB@users.noreply.github.com>
2025-06-16 23:13:08 +0200

    refactor: update HTML structure and enhance styling for color palette generator

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-let n;
+import "./style.css";
+
+/*
+Cuando generas colores en formato HEX (por ejemplo, #a1b2c3), necesitas 6 dígitos hexadecimales (0-9, a-f),
+ precedidos por #.
+Cada par representa rojo, verde y azul en hexadecimal:
+
+#RRGGBB
+Ejemplo: #12e4af
+Por lo tanto, debes generar un string de 6 caracteres hexadecimales aleatorios.
+*/
+const hexChars = "0123456789abcdef";
+
+function generatePalette() {
+	let rgb = "#";
+
+	for (let index = 0; index < 6; index++) {
+		rgb += randomChar(hexChars);
+	}
+	return rgb;
+}
+function randomChar(chars: string): string {
+	return chars[Math.floor(Math.random() * chars.length)];
+}
