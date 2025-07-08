e0774c1941582b525f34e4b3a9a053967af9a600
JavMB <javiermengual@live.com>
2025-07-08 21:55:45 +0200

    feat: add generate button and functionality to create color palettes

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,34 @@ Por lo tanto, debes generar un string de 6 caracteres hexadecimales aleatorios.
 const hexChars = "0123456789abcdef";
 
 function generatePalette() {
-  let rgb = "#";
+	let rgb = "#";
 
-  for (let index = 0; index < 6; index++) {
-    rgb += randomChar(hexChars);
-  }
-  return rgb;
+	for (let index = 0; index < 6; index++) {
+		rgb += randomChar(hexChars);
+	}
+	return rgb;
 }
 function randomChar(chars: string): string {
-  return chars[Math.floor(Math.random() * chars.length)];
+	return chars[Math.floor(Math.random() * chars.length)];
 }
+
+function generatePalettes(count: number): string[] {
+	const palettes: string[] = [];
+	for (let index = 0; index < count; index++) {
+		palettes.push(generatePalette());
+	}
+	return palettes;
+}
+
+const generateButton = document.getElementById("generate-button");
+
+generateButton?.addEventListener("click", () => {
+	const palettes = generatePalettes(5);
+
+	const colorBlocks = document.querySelectorAll<HTMLDivElement>(".color-block");
+
+	colorBlocks.forEach((block, i) => {
+		block.style.backgroundColor = palettes[i];
+	});
+	
+});
